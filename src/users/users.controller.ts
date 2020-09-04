c2d5dbbb1ea2c8228e20ddff141b4c0c4ae60a31
willikay11 <willikay11@gmail.com>
2020-09-04 16:36:24 +0300

    Added ability to get user by id

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Get, HttpStatus, Post, Query, Res } from '@nestjs/common';
+import { Controller, Get, HttpStatus, Param, Post, Query, Res } from '@nestjs/common';
 import { CommandBus, QueryBus } from '@nestjs/cqrs';
 import { ListUsersQuery } from './queries/list.query';
+import { GetUserByIdQuery } from './queries/getById.query';
 
 @Controller('api')
 export class UserController {
@@ -9,17 +10,30 @@ export class UserController {
     private readonly queryBus: QueryBus,
   ) {}
 
-@Get('/users')
-public async getAllUsers(
-    @Query() request: ListUsersQuery,
+  @Get('/users')
+  public async getAllUsers(
+      @Query() request: ListUsersQuery,
+      @Res() response
+    ) {
+    const users = await this.queryBus.execute(
+      new ListUsersQuery(
+        request.page,
+        request.pageSize
+      )
+    );
+
+    response.status(HttpStatus.OK).json(users);
+  }
+
+  @Get('/user/:id')
+  public async getUser(
+    @Param() param: GetUserByIdQuery,
     @Res() response
   ) {
-  const users = await this.queryBus.execute(
-    new ListUsersQuery(
-      request.page,
-      request.pageSize
-    )
-  );
+    const user = await this.queryBus.execute(
+      new GetUserByIdQuery(param.id)
+    );
 
-  response.status(HttpStatus.OK).json(users);
-}}
+    response.status(HttpStatus.OK).json(user);
+  }
+}
